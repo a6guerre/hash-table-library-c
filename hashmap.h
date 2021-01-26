813c670b6813af8c1d87ed5bcb621f11a1538cf3
John R. Patek <johnpatek2@gmail.com>
2021-01-26 15:59:30 -0800

    Forgot to add CMakeLists.txt

--- hashmap.h
+++ hashmap.h
@@ -27,8 +27,8 @@ typedef int (*Hashmap_traverse_cb) (HashmapNode *node);
 Hashmap *Hashmap_create(Hashmap_compare compare, Hashmap_hash);
 void Hashmap_destroy(Hashmap * map);
 
-int Hashmap_set(Hashmap * map, void *key, void *data);
-void *Hashmap_get(Hashmap * map, void *key);
+int Hashmap_set(Hashmap * map, char *key, void *data);
+void *Hashmap_get(Hashmap * map, char *key);
 
 int Hashmap_traverse(Hashmap * map, Hashmap_traverse_cb traverse_cb);
 
